78d1ae90e3236e0938cd79e50df00b21fb4e6e45
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-13 10:46:40 +0100

    Add addImageWatermarkToImage function to app

--- app.js
+++ app.js
@@ -8,8 +8,22 @@ const addTextWatermarkToImage = async function(inputFile, outputFile, text) {
     alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
     alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE,
   };
-  image.print(font, 10, 10, textData, image.getWidth(), image.getHeight());
+  image.print(font, 0, 0, textData, image.getWidth(), image.getHeight());
   await image.quality(100).writeAsync(outputFile);
 };
 
+const addImageWatermarkToImage = async function(inputFile, outputFile, watermarkFile) {
+    const image = await Jimp.read(inputFile);
+    const watermark = await Jimp.read(watermarkFile);
+    const x = image.getWidth() / 2 - watermark.getWidth() / 2;
+    const y = image.getHeight() / 2 - watermark.getHeight() / 2;
+  
+    image.composite(watermark, x, y, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacitySource: 0.5,
+    });
+    await image.quality(100).writeAsync(outputFile);
+  };
+
 addTextWatermarkToImage('./images/test.jpg', './images/test-with-watermark.jpg', 'Hello world');
+addImageWatermarkToImage('./images//test.jpg', './images/test-with-watermark-2.jpg', './images/logo.png');
